578f769573e555ac5efb4390b9290e6c0a6d29cb
Muhammad Alkady <kady.muhammad@gmail.com>
2020-06-16 23:47:00 +0200

    Playing with slidingUpPanelLayout offset and animation

--- app/src/main/java/com/kady/muhammad/quran/heritage/domain/log/Logger.kt
+++ app/src/main/java/com/kady/muhammad/quran/heritage/domain/log/Logger.kt
@@ -7,11 +7,11 @@ object Logger {
 
     private val isLoggingEnabled: Boolean = BuildConfig.IS_LOGGING_ENABLED
 
-    fun logE(tag: String, msg: String) {
-        if (isLoggingEnabled) Log.e(tag, msg)
+    fun logE(tag: String, msg: String, overrideIsLoggingEnabled: Boolean = isLoggingEnabled) {
+        if (overrideIsLoggingEnabled) Log.e(tag, msg)
     }
 
-    fun logI(tag: String, msg: String) {
-        if (isLoggingEnabled) Log.i(tag, msg)
+    fun logI(tag: String, msg: String, overrideIsLoggingEnabled: Boolean = isLoggingEnabled) {
+        if (overrideIsLoggingEnabled) Log.i(tag, msg)
     }
 }
--- app/src/main/java/com/kady/muhammad/quran/heritage/presentation/main/MainActivity.kt
+++ app/src/main/java/com/kady/muhammad/quran/heritage/presentation/main/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.kady.muhammad.quran.heritage.presentation.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.kady.muhammad.quran.heritage.R
+import com.kady.muhammad.quran.heritage.domain.log.Logger
 import com.kady.muhammad.quran.heritage.presentation.player.PlayerFragment
 import com.kady.muhammad.quran.heritage.presentation.vm.MediaViewModel
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SlidingUpPanelLayout.PanelSlideListener {
 
     val vm by lazy { ViewModelProvider(this).get(MediaViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSlidingPanel()
+        setupLogo()
+        panel.post {
+            syncNavHostFragmentOffset(getPanelOffset())
+        }
     }
 
     override fun onBackPressed() {
@@ -26,12 +33,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+        Logger.logI(
+            "onPanelSlide",
+            """"slidingOffset = $slideOffset | playerHeight = ${player.height} | navHostHeight = ${nav_host_fragment.height}""",
+            true
+        )
+        syncNavHostFragmentOffset(slideOffset)
+    }
+
+    override fun onPanelStateChanged(
+        panel: View,
+        previousState: SlidingUpPanelLayout.PanelState,
+        newState: SlidingUpPanelLayout.PanelState
+    ) {
+    }
+
     fun playPause(mediaId: String) {
         playerFragment().playPause(mediaId)
+    }
+
+    private fun syncNavHostFragmentOffset(slideOffset: Float) {
+        nav_host_fragment.translationY = -(nav_host_fragment.height * slideOffset)
+        nav_host_fragment.alpha = 1 - slideOffset
     }
 
     private fun playerFragment(): PlayerFragment {
         return supportFragmentManager.findFragmentById(R.id.player) as PlayerFragment
     }
+
+    private fun setupSlidingPanel() {
+        panel.addPanelSlideListener(this)
+    }
+
+    private fun setupLogo() {
+        logo.startAVDAnim()
+    }
+
+    private fun getPanelOffset() =
+        if (panel.panelState == SlidingUpPanelLayout.PanelState.EXPANDED) 1F else 0F
 
 }
