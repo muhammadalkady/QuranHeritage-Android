dac3ce26c20db9d2814fd2efe65fd3aa7a6b6851
Muhammad Alkady <kady.muhammad@gmail.com>
2020-06-24 00:03:23 +0200

    Custom font in custom notification

--- app/src/main/java/com/kady/muhammad/quran/heritage/domain/player/PlayerNotification.kt
+++ app/src/main/java/com/kady/muhammad/quran/heritage/domain/player/PlayerNotification.kt
@@ -8,6 +8,8 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Build
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
@@ -15,8 +17,10 @@ import android.support.v4.media.session.PlaybackStateCompat
 import android.widget.RemoteViews
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.media.session.MediaButtonReceiver
 import com.kady.muhammad.quran.heritage.R
+import com.kady.muhammad.quran.heritage.presentation.ext.textToBitmap
 import com.kady.muhammad.quran.heritage.presentation.main.MainActivity
 
 object PlayerNotification {
@@ -27,10 +31,10 @@ object PlayerNotification {
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createChannel(context: Context) {
         with(context) {
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            val name = getString(R.string.user_notification_channel_name)
-            val description = context.getString(R.string.user_notification_channel_desc)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationManager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val name: String = getString(R.string.user_notification_channel_name)
+            val description: String = context.getString(R.string.user_notification_channel_desc)
+            val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(NOTIFICATION_PLAYER_NOTIFICATION_CHANNEL_ID, name, importance)
             channel.description = description
             channel.setShowBadge(false)
@@ -50,33 +54,42 @@ object PlayerNotification {
             val bigNotificationLayout = RemoteViews(packageName, R.layout.player_notification_big)
             val smallNotificationLayout =
                 RemoteViews(packageName, R.layout.player_notification_small)
-            val customNotificationBuilder =
+            val customNotificationBuilder: NotificationCompat.Builder =
                 NotificationCompat.Builder(this, NOTIFICATION_PLAYER_NOTIFICATION_CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_notification)
-                    .setContentIntent(PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), 0))
+                    .setContentIntent(
+                        PendingIntent.getActivity(
+                            this, 0,
+                            Intent(this, MainActivity::class.java), 0
+                        )
+                    )
                     .setCustomBigContentView(bigNotificationLayout)
                     .setCustomContentView(smallNotificationLayout)
                     .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             //
-            val playPauseImageRes = if (paused) R.drawable.ic_play_arrow else R.drawable.ic_pause
+            val accentColor: Int = ContextCompat.getColor(applicationContext, R.color.colorAccent)
+            //
+            val playPauseImageRes: Int = if (paused) R.drawable.ic_play_arrow else R.drawable.ic_pause
             bigNotificationLayout.setImageViewResource(R.id.playPause, playPauseImageRes)
             smallNotificationLayout.setImageViewResource(R.id.playPause, playPauseImageRes)
             //
-            val pendingIntent = if (paused) MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY)
+            val pendingIntent: PendingIntent = if (paused) MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY)
             else MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PAUSE)
             bigNotificationLayout.setOnClickPendingIntent(R.id.playPause, pendingIntent)
             smallNotificationLayout.setOnClickPendingIntent(R.id.playPause, pendingIntent)
             //
-            val title = mediaSession.controller.metadata.getString(MediaMetadataCompat.METADATA_KEY_TITLE)
-            bigNotificationLayout.setTextViewText(R.id.trackName, title)
-            smallNotificationLayout.setTextViewText(R.id.trackName, title)
+            val title: String = mediaSession.controller.metadata.getString(MediaMetadataCompat.METADATA_KEY_TITLE)
+            val titleBitmap: Bitmap = textToBitmap(title, 48F, accentColor, true)
+            bigNotificationLayout.setImageViewBitmap(R.id.trackName, titleBitmap)
+            smallNotificationLayout.setImageViewBitmap(R.id.trackName, titleBitmap)
             //
-            val displayTitle = mediaSession.controller.metadata.getString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE)
-            bigNotificationLayout.setTextViewText(R.id.playListName, displayTitle)
-            smallNotificationLayout.setTextViewText(R.id.playListName, displayTitle)
+            val displayTitle: String = mediaSession.controller.metadata.getString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE)
+            val displayTitleBitmap: Bitmap = textToBitmap(displayTitle, 48F, Color.WHITE, true)
+            bigNotificationLayout.setImageViewBitmap(R.id.playListName, displayTitleBitmap)
+            smallNotificationLayout.setImageViewBitmap(R.id.playListName, displayTitleBitmap)
             //
-            val customNotification = customNotificationBuilder.build()
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val customNotification: Notification = customNotificationBuilder.build()
+            val notificationManager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(NOTIFICATION_ID, customNotification)
             return@with customNotification
         }
--- app/src/main/java/com/kady/muhammad/quran/heritage/presentation/ext/Ext.kt
+++ app/src/main/java/com/kady/muhammad/quran/heritage/presentation/ext/Ext.kt
@@ -2,7 +2,12 @@ package com.kady.muhammad.quran.heritage.presentation.ext
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.view.View
+import com.kady.muhammad.quran.heritage.presentation.font.Font
 
 fun View.show(onEnd: () -> Unit = {}) {
     animate().alpha(1F).setDuration(300).setListener(object : AnimatorListenerAdapter() {
@@ -14,4 +19,20 @@ fun View.hide(onEnd: () -> Unit = {}) {
     animate().alpha(0F).setDuration(300).setListener(object : AnimatorListenerAdapter() {
         override fun onAnimationEnd(animation: Animator) = onEnd()
     }).start()
+}
+
+fun textToBitmap(messageText: String, textSize: Float, textColor: Int, isBold: Boolean = true): Bitmap {
+    val paint = Paint()
+    paint.textSize = textSize
+    paint.typeface = Typeface.create(Font.main, if (isBold) Typeface.BOLD else Typeface.NORMAL)
+    paint.color = textColor
+    paint.textAlign = Paint.Align.RIGHT
+    paint.isAntiAlias = true
+    val baseline: Float = -paint.ascent()
+    val width: Int = (paint.measureText(messageText) + 0.5f).toInt()
+    val height: Int = (baseline + paint.descent() + 0.5f).toInt()
+    val image: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(image)
+    canvas.drawText(messageText, width.toFloat(), baseline, paint)
+    return image
 }
