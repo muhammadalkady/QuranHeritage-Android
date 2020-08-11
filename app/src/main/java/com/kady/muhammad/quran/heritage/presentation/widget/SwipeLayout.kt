07b5a101ca4fab0611ac8b45be329a8ad1300229
Muhammad Alkady <kady.muhammad@gmail.com>
2020-08-11 14:17:04 +0200

    Fix UI bugs

--- app/src/main/java/com/kady/muhammad/quran/heritage/presentation/widget/SwipeLayout.kt
+++ app/src/main/java/com/kady/muhammad/quran/heritage/presentation/widget/SwipeLayout.kt
@@ -6,17 +6,19 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.kady.muhammad.quran.heritage.domain.log.Logger
+import kotlin.math.abs
 
 class SwipeLayout : FrameLayout {
 
     var disableSwipe = false
 
     private val tag = "SwipeLayout"
-    private val isLoggingEnabled = false
+    private val isLoggingEnabled = true
     private val animationDuration = 150L
-    private val swipeXThreshold = 100
     private var dismissListener: (() -> Unit)? = null
     private var swipeListener: ((fraction: Float) -> Unit)? = null
+    private var firstDistanceX: Float = Float.MIN_VALUE
+    private var firstDistanceY: Float = Float.MIN_VALUE
     private val gestureDetector: GestureDetector by lazy { GestureDetector(context, gestureDetectorListener) }
     private val gestureDetectorListener: GestureDetector.OnGestureListener = object : GestureDetector.OnGestureListener {
         override fun onShowPress(e: MotionEvent?) {
@@ -39,10 +41,17 @@ class SwipeLayout : FrameLayout {
         }
 
         override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-            Logger.logI(tag, "onScroll e2.rawX = ${e2?.x} | distanceX = $distanceX", isLoggingEnabled)
+            Logger.logI(tag, "onScroll e2.rawX = ${e2?.x} | distanceX = $firstDistanceX |  distanceY = $firstDistanceY", true)
             e1 ?: return false
             e2 ?: return false
-            if (e1.rawX < swipeXThreshold) x = e2.rawX
+            if (firstDistanceX == Float.MIN_VALUE && firstDistanceY == Float.MIN_VALUE) {
+                firstDistanceX = distanceX
+                firstDistanceY = distanceY
+            }
+            if (abs(firstDistanceY) > abs(firstDistanceX)) return false
+            val newX: Float = e2.rawX - e1.rawX
+            x = if (newX < 0F) 0F else newX
+            Logger.logI(tag, "onScroll x = $x , e1.rawX = ${e1.rawX} , e2.rawX = ${e2.rawX}", false)
             return false
         }
 
@@ -79,6 +88,8 @@ class SwipeLayout : FrameLayout {
     }
 
     private fun onUpTouch() {
+        firstDistanceX = Float.MIN_VALUE
+        firstDistanceY = Float.MIN_VALUE
         val dismissPoint: Float = width.div(other = 2F)
         if (x >= dismissPoint) {
             animate()
